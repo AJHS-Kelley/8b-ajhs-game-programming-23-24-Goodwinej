dc5527986a847a5c7689cb0d1164f38b95e736a4
Goodwinej <142345252+Goodwinej@users.noreply.github.com>
2023-10-17 14:29:24 -0400

    Update collections.py

--- gameProgramming/gaming_exercises/00_numberGuess/02_collections/collections.py
+++ gameProgramming/gaming_exercises/00_numberGuess/02_collections/collections.py
@@ -1,14 +1,31 @@
-# ADDING ITEMS
-playerInventory = []
-while  len(playerInventory)<10:
-    item = input("What would you like to add?\n")
-    playerInventory.append(item) # .append() adds to END of List.
-playerInventory.sort()
-# .whatever() is known as a METHOD. It means "do this to that".
-print(playerInventory)
+# # ADDING ITEMS
+# playerInventory = []
+# while  len(playerInventory)<10:
+#     item = input("What would you like to add?\n")
+#     playerInventory.append(item) # .append() adds to END of List.
+# playerInventory.sort()
+# # .whatever() is known as a METHOD. It means "do this to that".
+# print(playerInventory)
 
-# REMOVE ITEMS
-while  len(playerInventory) > 5:
-    item = input("What would you like to remove?\n")
-    playerInventory.remove(item)
-playerInventory.sort()     
+# # REMOVE ITEMS
+# while  len(playerInventory) > 5:
+#     item = input("What would you like to remove?\n")
+#     playerInventory.remove(item)
+# playerInventory.sort()
+# print(playerInventory)
+
+
+# doorKeys = [
+#     "red",
+#     "green",
+#     "pink",
+#     "blue",
+#     "black"
+# ]
+
+# key = input("Which color key do you need to unlock the door?\n")
+# if key in doorKeys:
+#     print("You have the correct key! The door unlocks\n")
+# else:
+#     print("You do not have that key. The door remains locked.")    
+
