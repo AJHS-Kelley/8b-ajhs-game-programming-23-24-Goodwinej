34704ecff974aba72d552184377803738f03e4ca
Goodwinej <142345252+Goodwinej@users.noreply.github.com>
2023-10-31 14:17:15 -0400

    made the game board.
    MAde every possible outcome for the game board.

--- gameProgramming/04B_hangMan/hangman.py
+++ gameProgramming/04B_hangMan/hangman.py
@@ -1,4 +1,47 @@
 #Hangman Game by Jermya Goodwine
 
 words ='big ink kid tin cat dog hot men ice red cactus judge smart larger marker equal pulp smoke easy magical conjunctively extemporizing dactylography electroencephalographically hyperimmunize physicalizing maximizations yellowjackets methylbenzene tranquillizer alphabetizing'.split()
-print(words)
+
+# VARIABLE_NAMES IN ALL-CAPS ARE CONSTANTS THAT DON"T CHANGE
+HANGMAN_BOARD = ['''
+    +---+             
+        |        
+        |
+        |         
+   ==========''','''
+    +---+             
+    0   |        
+        |
+        |
+   ==========''','''
+    +---+             
+    0   |        
+    |   |
+        |
+   ==========''','''
+    +---+             
+    0   |        
+   /|   |
+        |
+   ==========''','''
+    +---+             
+    0   |        
+   /|\  |
+        |
+   ==========''','''
+    +---+             
+    0   |        
+   /|\  |
+   /    |
+   ==========''','''
+    +---+             
+    0   |        
+   /|\  |
+   / \  |
+   ==========''']
+
+i = 0
+while i< len(HANGMAN_BOARD):
+    print(HANGMAN_BOARD[i])
+    i += 1
+    
