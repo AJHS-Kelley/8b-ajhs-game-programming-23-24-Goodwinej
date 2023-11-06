68d48546be8c8eff4e2bf0fbac949ddcbc14106b
Goodwinej <142345252+Goodwinej@users.noreply.github.com>
2023-11-06 14:19:43 -0500

    Update hangman.py

--- gameProgramming/04B_hangMan/hangman.py
+++ gameProgramming/04B_hangMan/hangman.py
@@ -1,6 +1,6 @@
 #Hangman Game by Jermya Goodwine
 import random
-words ='big ink kid tin cat dog hot men ice red cactus judge smart larger marker equal pulp smoke easy magical conjunctively extemporizing dactylography electroencephalographically hyperimmunize physicalizing maximizations yellowjackets methylbenzene tranquillizer alphabetizing'.split()
+#words ='big ink kid tin cat dog hot men ice red cactus judge smart larger marker equal pulp smoke easy magical conjunctively extemporizing dactylography electroencephalographically hyperimmunize physicalizing maximizations yellowjackets methylbenzene tranquillizer alphabetizing'.split()
 #
 #
 #
@@ -45,13 +45,32 @@ HANGMAN_BOARD = ['''
     0   |        
    /|\  |
    / \  |
-   ==========''']
+   ==========''','''    
+   +---+             
+    0   |        
+  0-|-0 |
+   / \  |
+   ==========]''',''' 
+   +---+             
+    0   |        
+  0-|-0 |
+   / \  |
+  0   0 |
+   ==========]''']
 
 #PICK Word from the list
 def getRandomWord(wordList): # return a radom word from the list.
    wordIndex = random.randint(0, len(wordList) - 1)
    # len(Listname) -1 is EXTREMLY COMMON FOR WORKING WITH LISTS.
    return wordList[wordIndex]
+
+# Pick from the dictionary
+def getRandomWord(wordDict): # return a radom word from the list.
+   wordKey = random.choice(list(wordDict.keys()))
+   wordIndex = random.randint(0, len(wordDict[wordKey]) - 1)
+   return [wordDict[wordKey][wordIndex], wordKey]
+   
+   # len(Listname) -1 is EXTREMLY COMMON FOR WORKING WITH LISTS.
 
 def displayBoard(missedLetters, correctLetters, secretWord):
     print(HANGMAN_BOARD[len(missedLetters)])
