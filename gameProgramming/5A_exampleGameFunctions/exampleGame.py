4eb04e32a9381d62cb8558635d687ca97fa62368
Goodwinej <142345252+Goodwinej@users.noreply.github.com>
2023-11-13 14:42:57 -0500

    Create exampleGame.py

---
+++ gameProgramming/5A_exampleGameFunctions/exampleGame.py
@@ -0,0 +1,21 @@
+#Jermya goodwine, Tracksim, v0.0
+import random
+
+def starterPistol():
+    input("are you ready?\n press enter")
+    print("1..")
+    print("2..")
+    print("3!!!")
+    print("**WHistle sound**")
+    pass
+
+def selectEvent():
+    pass
+
+def hurdles():
+    pass
+
+def longJump():
+    pass
+
+
