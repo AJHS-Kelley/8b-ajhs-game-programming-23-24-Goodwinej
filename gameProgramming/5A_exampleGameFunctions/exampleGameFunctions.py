8b38de0164815c7fb3d712d0b56f34df1641a31b
Goodwinej <142345252+Goodwinej@users.noreply.github.com>
2023-11-13 14:04:33 -0500

    Update exampleGameFunctions.py

--- gameProgramming/5A_exampleGameFunctions/exampleGameFunctions.py
+++ gameProgramming/5A_exampleGameFunctions/exampleGameFunctions.py
@@ -1,1 +1,14 @@
-# jermya goodwine, example game functions.
+# jermya goodwine, example game functions, v0.0
+import random
+
+def functionOne():
+    pass
+
+def functionTwo(param1):
+    pass
+
+def functionThree(param1 = "Default VAlue"):
+    pass
+
+def functionFour(param1, param2, param3):
+    pass
