3342f0b0150560509c88135eedd5a97d7c68bc08
Goodwinej <142345252+Goodwinej@users.noreply.github.com>
2023-11-30 13:38:36 -0500

    Update exampleGame.py

--- gameProgramming/5A_exampleGameFunctions/exampleGame.py
+++ gameProgramming/5A_exampleGameFunctions/exampleGame.py
@@ -7,15 +7,24 @@ def starterPistol():
     print("2..")
     print("3!!!")
     print("**WHistle sound**")
-    pass
+    return
 
-def selectEvent():
-    pass
+def run(speed, startquality, indurance):
+    if startquality >= 6 and speed >= 14.5 and indurance >= 5:
+        firstPlace = True
+        print('You got first place, what an amazing runner!')
+    elif startquality >= 5 and speed >= 12 and indurance >= 3:
+        secondPlace = True
+        print('You got second place, congrats on coming second!')
+    else:
+        print('Oh no last place ig.')
+        firstPlace = False
+        secondPlace = False
 
-def hurdles():
-    pass
+def hurdles(speed, reactionTime, jumpHeight):
+    if jumpHeight >= 10 and reactionTime <= 2.5 and speed >= 14.5:
+        firstPLace= True
+        
 
-def longJump():
-    pass
 
 
