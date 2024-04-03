c10a3d0719386e972427d6c788087b73d84ca3a1
Goodwinej <142345252+Goodwinej@users.noreply.github.com>
2024-04-03 14:11:02 -0400

    Create sliceDice.py

---
+++ gameProgramming/abc.py/11_sliceDice/sliceDice.py
@@ -0,0 +1,26 @@
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+mystring = "you are crazy dude. \n"
+
+print(alphabet[0])
+print(alphabet[-1])
+
+# Slicing
+print(alphabet[0:4]) # Slice UP to second index.
+# alphabet [star:stop]
+
+# Slice from the start
+print(alphabet[:9])
+
+# Slice to the End
+print(alphabet[12:])
+
+# Slice the Whole %#&%* Thing
+print(alphabet[:])
+
+# Negative Index Slicing
+print(alphabet[-13:-1])
+
+print(mystring[0:4])
+print(mystring[4:8])
+print(mystring[8:])
+print(mystring[:])
